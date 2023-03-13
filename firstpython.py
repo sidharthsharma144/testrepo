afa88a513e03d65aec5ad3102ea3fa6e8df8574d
chikkisharma2004 <126665387+chikkisharma2004@users.noreply.github.com>
2023-03-13 16:02:13 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -2,3 +2,4 @@ print("new file")
 print("hello sidharth sharma! this line was added by sidharth thakur to confirm the collab")
 print("have a nice day ahead!")
 print("OK fuck Off")
+helloo sidharth
